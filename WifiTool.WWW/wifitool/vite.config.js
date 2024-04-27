6f1469267bb9d328360b7976aacc3f75b3a0932a
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-04-27 12:45:18 +0300

    Update Build Tool

--- WifiTool.WWW/wifitool/vite.config.js
+++ WifiTool.WWW/wifitool/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '..\\..\\Wifi Extract IV\\WWW'
+    outDir: '..\\..\\WifiTool\\WWW'
   }
 })
